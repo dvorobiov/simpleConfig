5f238a0720096686f587c08730aaa26a18705793
dvorobiov <2belikespring@gmail.com>
2012-11-05 23:37:12 +0300

    init commit

---
+++ simpleConfig.js
@@ -0,0 +1,31 @@
+
+exports = module.exports = config;
+
+var config = {}
+	, currentEnvironment = process.env.ENV_VARIABLE || 'development';
+	, config.settings = {};
+
+config.configure = function(env, fn){
+  var envs = 'all'
+    , args = [].slice.call(arguments);
+
+  fn = args.pop();
+  if (args.length) envs = args;
+  if ('all' == envs || ~envs.indexOf(currentEnvironment)) fn.call(this);
+  return this;
+};
+
+config.set = function(setting, val){
+  if (1 == arguments.length) {
+      return this.settings[setting];
+  } else {
+    this.settings[setting] = val;
+    return this;
+  }
+};
+
+config.get = function(setting) {
+	return this.settings[setting];
+};
+
+
